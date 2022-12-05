7d25d12da6661553f23c0622a55a2af75b77bc3f
GIT DE FELIPE <felipeignacioxx@gmail.com>
2022-12-05 11:18:14 -0300

    modifcación menu y transsicion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
-// $(document).scroll(function() {
-//     const y = $("html").scrollTop();
+$(document).scroll(function() {
+    const y = $("html").scrollTop();
 
+    y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
 
-//     y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
-
-
-// })
+})
 
 
 // habilitacion tooltips boostrap
