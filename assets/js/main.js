398850eeac6fd149f314dd7b746f0cb6df06c110
GIT DE FELIPE <felipeignacioxx@gmail.com>
2022-12-05 12:16:22 -0300

    se modifica main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).scroll(function() {
     const y = $("html").scrollTop();
 
-    y > 150 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
+    y > 80 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
 
 })
 
